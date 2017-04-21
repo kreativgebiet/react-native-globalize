1b366869297472bf4802ab70ed9eff409df1be0e
Malte Boeing <m.boeing@appcom-interactive.de>
2017-04-21 11:35:48 +0200

    update cldr to inc naira

--- gulpfile.js
+++ gulpfile.js
@@ -13,67 +13,16 @@ const path = require('path');
 const Cldr = require('cldrjs');
 
 const locales = [
-  'am',           // Amharic
-  'ar',           // Arabic
-  'bg',           // Bulgarian
-  'bn',           // Bengali
-  'ca',           // Catalan
-  'cs',           // Czech
-  'da',           // Danish
-  'de',           // German
-  'el',           // Greek
-  'en',           // English (United States)
-  'en-GB',        // English (Great Britain)
-  'es',           // Spanish
-  'es-419',       // Spanish (Latin America & Caribbean)
-  'et',           // Estonian
-  'fa',           // Persian
-  'fi',           // Finnish
-  'fil',          // Filipino
-  'fr',           // French
-  'gu',           // Gujarati
-  'he',           // Hebrew
-  'hi',           // Hindi
-  'hr',           // Croatian
-  'hu',           // Hungarian
-  'id',           // Indonesian
-  'it',           // Italian
-  'ja',           // Japanese
-  'kn',           // Kannada
-  'ko',           // Korean
-  'lt',           // Lithuanian
-  'lv',           // Latvian
-  'ml',           // Malayalam
-  'mr',           // Marathi
-  'ms',           // Malay
-  'nb',           // Norwegian
-  'nl',           // Dutch
-  'pl',           // Polish
-  'pt',           // Portuguese
-  'pt-PT',        // Portuguese (Portugal)
-  'ro',           // Romanian
-  'ru',           // Russian
-  'sk',           // Slovak
-  'sl',           // Slovenian
-  'sr',           // Serbian
-  'sv',           // Swedish
-  'sw',           // Swahili
-  'ta',           // Tamil
-  'te',           // Telugu
-  'th',           // Thai
-  'tr',           // Turkish
-  'uk',           // Ukrainian
-  'vi',           // Vietnamese
-  'zh',           // Chinese
-  'zh-Hans',      // Chinese (Simplified)
-  'zh-Hant',      // Chinese (Traditional)
+    'de',           // German
+    'en',           // English (United States)
+    'ng',           // Nigeria
 ];
 
 const currencies = [
-  'CAD',          // Canadian Dollar
-  'EUR',          // Euro
-  'GBP',          // British Pound
-  'USD',          // US Dollar
+    'EUR',          // Euro
+    'GBP',          // British Pound
+    'USD',          // US Dollar
+    'NGN',          // Nigerian Naira
 ];
 
 const files = ['ca-gregorian', 'currencies', 'dateFields', 'numbers', 'timeZoneNames'];
@@ -82,65 +31,65 @@ const cldrs = locales.map(x => new Cldr(x));
 const languages = cldrs.map(x => x.attributes.language);
 
 function removeUnusedLanguages(dict) {
-  if (dict) {
-    Object.keys(dict).forEach((key) => {
-      if (languages.indexOf(key) === -1) {
-        delete dict[key];
-      }
-    });
-  }
+    if (dict) {
+        Object.keys(dict).forEach((key) => {
+            if (languages.indexOf(key) === -1) {
+                delete dict[key];
+            }
+        });
+    }
 }
 
 gulp.task('cldr', () => {
-  const cldrFilter = filter(file => (
-    (locales.indexOf(path.dirname(file.path).split(path.sep).pop()) > -1 && files.indexOf(path.basename(file.path, '.json')) > -1) || (path.dirname(file.path).split(path.sep).pop() === 'supplemental' && supplemental.indexOf(path.basename(file.path, '.json')) > -1)
-  ));
+    const cldrFilter = filter(file => (
+        (locales.indexOf(path.dirname(file.path).split(path.sep).pop()) > -1 && files.indexOf(path.basename(file.path, '.json')) > -1) || (path.dirname(file.path).split(path.sep).pop() === 'supplemental' && supplemental.indexOf(path.basename(file.path, '.json')) > -1)
+    ));
 
-  return gulp.src(['./node_modules/cldr-data/supplemental/*.json', './node_modules/cldr-data/main/**/*.json'])
-    .pipe(cldrFilter)
-    .pipe(merge('cldr.json', (obj) => {
-      if (obj.main) {
-        // For language files, grab the first language, and filter stuff out
-        const key = Object.keys(obj.main)[0];
-        const data = obj.main[key];
+    return gulp.src(['./node_modules/cldr-data/supplemental/*.json', './node_modules/cldr-data/main/**/*.json'])
+        .pipe(cldrFilter)
+        .pipe(merge('cldr.json', (obj) => {
+            if (obj.main) {
+                // For language files, grab the first language, and filter stuff out
+                const key = Object.keys(obj.main)[0];
+                const data = obj.main[key];
 
-        // Cut out unused dates.timeZoneNames.zone and dates.timeZoneNames.metazone data
-        if (data && data.dates && data.dates.timeZoneNames) {
-          data.dates.timeZoneNames.zone = {};
-          data.dates.timeZoneNames.metazone = {};
-        }
+                // Cut out unused dates.timeZoneNames.zone and dates.timeZoneNames.metazone data
+                if (data && data.dates && data.dates.timeZoneNames) {
+                    data.dates.timeZoneNames.zone = {};
+                    data.dates.timeZoneNames.metazone = {};
+                }
 
-        // Only include above currencies in each language
-        if (data && data.numbers && data.numbers.currencies) {
-          Object.keys(data.numbers.currencies).forEach((code) => {
-            if (currencies.indexOf(code) === -1) {
-              delete data.numbers.currencies[code];
+                // Only include above currencies in each language
+                if (data && data.numbers && data.numbers.currencies) {
+                    Object.keys(data.numbers.currencies).forEach((code) => {
+                        if (currencies.indexOf(code) === -1) {
+                            delete data.numbers.currencies[code];
+                        }
+                    });
+                }
             }
-          });
-        }
-      }
 
-      // Cut out unused languages from our supplemental files
-      if (obj.supplemental) {
-        const languageDictKeys = ['plurals-type-ordinal', 'plurals-type-cardinal'];
+            // Cut out unused languages from our supplemental files
+            if (obj.supplemental) {
+                const languageDictKeys = ['plurals-type-ordinal', 'plurals-type-cardinal'];
 
-        languageDictKeys.forEach((languageDictKey) => {
-          removeUnusedLanguages(obj.supplemental[languageDictKey]);
-        });
+                languageDictKeys.forEach((languageDictKey) => {
+                    removeUnusedLanguages(obj.supplemental[languageDictKey]);
+                });
 
-        // Only include currencies above
-        if (obj.supplemental.currencyData) {
-          delete obj.supplemental.currencyData.region;
+                // Only include currencies above
+                if (obj.supplemental.currencyData) {
+                    delete obj.supplemental.currencyData.region;
 
-          Object.keys(obj.supplemental.currencyData.fractions).forEach((code) => {
-            if (currencies.indexOf(code) === -1 && code.toLowerCase() !== 'default') {
-              delete obj.supplemental.currencyData.fractions[code];
+                    Object.keys(obj.supplemental.currencyData.fractions).forEach((code) => {
+                        if (currencies.indexOf(code) === -1 && code.toLowerCase() !== 'default') {
+                            delete obj.supplemental.currencyData.fractions[code];
+                        }
+                    });
+                }
             }
-          });
-        }
-      }
 
-      return obj;
-    }))
-    .pipe(gulp.dest('lib'));
+            return obj;
+        }))
+        .pipe(gulp.dest('lib'));
 });
